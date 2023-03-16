2390eb465f2b5516c9fbd7111f336b0f5d0c9515
Elève <Elve@172.17.45.97>
2023-03-16 10:51:57 +0100

    partage de tout le projet

---
+++ src/main/java/fr/insa/wahl/projet_56/Revetement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.wahl.projet_56;
+
+/**
+ *
+ * @author mwahl01
+ */
+public class Revetement {
+    int idRevetement ;
+    double prix ;
+    
+    Revetement(int id, double p)
+    {
+        this.idRevetement=id;
+        this.prix=p;
+    }
+}
