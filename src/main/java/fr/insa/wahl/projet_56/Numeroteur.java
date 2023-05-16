86545ddd9643a9c8b2de2497cb55f7e4554a0e1d
Elève <Elve@UTILISA-V1Q5JOJ>
2023-05-16 14:05:05 +0200

    partage de tout le projet

---
+++ src/main/java/fr/insa/wahl/projet_56/Numeroteur.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.wahl.projet_56;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author Elève
+ * @param <Obj>
+ */
+public class Numeroteur <Obj> {
+    
+    private List<Obj> objets;
+    private HashMap<Obj,Integer> indexes;
+
+    public Numeroteur() {
+        this(new ArrayList<Obj>());
+    }
+
+    public Numeroteur(Collection<Obj> objets) {
+        this.objets = new ArrayList<>();
+        this.indexes = new HashMap<>(this.objets.size());
+        for (int i = 0 ; i < this.objets.size() ; i ++) {
+            this.indexes.put(this.objets.get(i), i);
+        }
+    }
+    
+    public int getOrAdd(Obj obj) {
+        Integer res = this.indexes.get(obj);
+        if (res == null) {
+            res = this.objets.size();
+            this.indexes.put(obj, res);
+            this.objets.add(obj);
+        }
+        return res;
+    }
+     
+     /**
+     * retourne l'objet correspondant à un index.
+     * @param index int
+     * @return Object
+     */
+    public Obj getObject(int index) {
+        Obj res = this.objets.get(index);
+        if (res == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        return res;
+    }
+
+    /**
+     * retourne tous les objets contenus dans le numéroteur.
+     * @return Variable[] l'ensemble des variables du numéroteur
+     */
+    public Collection<Obj> getAllObjects() {
+        return this.objets;
+    }
+
+    /**
+     * retourne l'index correspondant à une variable.
+     * @param var Variable var
+     * @return int
+     */
+    public int getIndex(Obj var) {
+        Integer res = this.indexes.get(var);
+        if(res == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return "Numeroteur{" + "objets=" + objets + ", indexes=" + indexes + '}';
+    }
+
+    public boolean add(Obj obj) {
+        if (this.indexes.get(obj) == null) {
+            this.indexes.put(obj, this.objets.size());
+            this.objets.add(obj);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
