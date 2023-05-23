24da00d04114eb3ee296a25c7da7d102620a9e52
Elève <Elve@UTILISA-V1Q5JOJ>
2023-05-23 11:31:36 +0200

    partage de tout le projet

--- src/main/java/fr/insa/wahl/projet_56/Batiment.java
+++ src/main/java/fr/insa/wahl/projet_56/Batiment.java
@@ -1,5 +1,7 @@
 package fr.insa.wahl.projet_56;
 
+import java.io.*;
+
 public class Batiment {
     private String idBatiment;
     private Numeroteur<Niveau> listeNiveau;
@@ -15,5 +17,13 @@ public class Batiment {
     
     public Numeroteur<Niveau> getListe(){
         return listeNiveau;
+    }
+    
+    public void sauvegarde(){
+        try{
+            BufferedWriter batiment=new BufferedWriter(new FileWriter("Batiment.txt",true));
+            }
+        catch (IOException err)
+        {System.out.println("Erreur :\n"+err);}
     }
 }
